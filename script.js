1c86755a2ae76a0c1f580b8eebbae643b55f3658
asdqwertzy <asdf666@mail.com>
2023-06-26 23:19:34 +0200

    Add finished game

--- script.js
+++ script.js
@@ -4,4 +4,59 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
+function start(choice) {
+    const pc = getComputerChoice();
+    const human = choice.toLowerCase();
+    const outcomes = {
+        rock: {
+            rock: "Tie! Both chose Rock.",
+            paper: "You win! Paper beats Rock!",
+            scissors: "You lose! Rock beats Scissors!"
+        },
+        paper: {
+            rock: "You lose! Paper beats Rock!",
+            paper: "Tie! Both chose Paper.",
+            scissors: "You win! Scissors beats Paper!"
+        },
+        scissors: {
+            rock: "You win! Rock beats Scissors!",
+            paper: "You lose! Scissors beats Paper!",
+            scissors: "Tie! Both chose Scissors."
+        }
+    };
+    return outcomes[pc][human];
+}
 
+function game() {
+    var pc = 0, human = 0; 
+    choices = ["rock", "paper", "scissors" ]
+    for(i = 0; i < 5; i++)
+    {
+        choice = prompt("Please pick one: Rock, paper, or scissors.")
+        if (!choices.includes(choice.toLowerCase())) {
+            console.log("Invalid choice, try again.")
+            continue;
+        }
+        else 
+        {
+            str = start(choice);
+            if (str.includes("win")) { 
+                human++;
+                console.log(str)
+                console.log(`${human}-${pc}`);
+            }
+            if (str.includes("lose")) { 
+                pc++;
+                console.log(str)
+                console.log(`${human}-${pc}`);
+            }
+            if (str.includes("Tie")) {
+                console.log(str)
+                console.log(`${human}-${pc}`);
+            }
+        }
+    }
+    if (human > pc) { return(`Winner! ${human}-${pc}`); }
+    else if (human < pc) { return(`Loser! ${human}-${pc}`); }
+    else return (`Tie! ${human}-${pc}`);
+}
