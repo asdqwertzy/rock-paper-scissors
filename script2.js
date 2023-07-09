8a79fa426c5068568e8f12e01aa32acd715d5168
asdqwertzy <asdf666@mail.com>
2023-07-09 12:47:30 +0200

    Add nudge effect to score info elements

--- script2.js
+++ script2.js
@@ -1,3 +1,23 @@
+function nudge(element) {
+  if (element instanceof NodeList) {
+    element.forEach(function(el) {
+      el.classList.toggle('nudge');
+      el.classList.toggle('nudge-animation');
+      setTimeout(function() {
+        el.classList.toggle('nudge');
+        el.classList.toggle('nudge-animation');
+      }, 100)
+    });
+  } else {
+    element.classList.toggle('nudge');
+    element.classList.toggle('nudge-animation');
+    setTimeout(function() {
+        element.classList.toggle('nudge');
+        element.classList.toggle('nudge-animation');
+      }, 100)
+  }
+}
+
 function getComputerChoice() {
     var choices = ["Rock", "Paper", "Scissors"]
     var randomIndex = Math.floor(Math.random() * choices.length);
@@ -8,7 +28,8 @@ function play(choice) {
     var comp = getComputerChoice()
     if (choice === comp) {
         scoremsg.textContent = "Tie."
-        document.querySelector(".info").textContent = `${comp} and ${choice}`;
+        nudge(scoremsg)
+        info.textContent = `${comp} and ${choice}`;
     } else if (
         (choice === "Rock" && comp === "Scissors") ||
         (choice === "Paper" && comp === "Rock") ||
@@ -18,25 +39,27 @@ function play(choice) {
         humanscore.textContent = "You: " + human;
         scoremsg.textContent = "You win the round."
         if (choice == "Scissors" && comp == "Paper") {
-            document.querySelector(".info").textContent = `${choice} beat ${comp}`;
+            info.textContent = `${choice} beat ${comp}`;
         }
         else {
-            document.querySelector(".info").textContent = `${choice} beats ${comp}`;
+            info.textContent = `${choice} beats ${comp}`;
         }
+        nudge(humanscore)
     } else {
         pc++;
         pcscore.textContent = "Computer: " + pc;
         scoremsg.textContent = "Computer wins the round."
         if (comp == "Scissors" && choice == "Paper") {
-            document.querySelector(".info").textContent = `${comp} beat ${choice}`;
+            info.textContent = `${comp} beat ${choice}`;
         }
         else {
-            document.querySelector(".info").textContent = `${comp} beats ${choice}`;
+            info.textContent = `${comp} beats ${choice}`;
         }
+        nudge(pcscore)
     }
     if (human >= 5) {
         scoremsg.textContent = "You win."
-        document.querySelector(".info").textContent = `${human} - ${pc}`
+        info.textContent = `${human} - ${pc}`
         pc = 0
         human = 0
         started = 0
@@ -44,14 +67,14 @@ function play(choice) {
         pcscore.textContent = "Computer: 0"
         
         scoreboard.forEach(scoreboard => { scoreboard.style.visibility = "hidden" })
-        document.querySelector(".info").style.visibility = "visible"
+        info.style.visibility = "visible"
         scoremsg.style.visibility = "visible"
         buttons[0].style.visibility = "visible"
         hidechoices()
     }
     else if (pc >= 5) {
         scoremsg.textContent = "You lose."
-        document.querySelector(".info").textContent = `${human} - ${pc}`
+        info.textContent = `${human} - ${pc}`
         pc = 0
         human = 0
         started = 0
@@ -59,7 +82,7 @@ function play(choice) {
         pcscore.textContent = "Computer: 0"
         scoreboard.forEach(scoreboard => { scoreboard.style.visibility = "hidden" })
         scoremsg.style.visibility = "visible"
-        document.querySelector(".info").style.visibility = "visible"
+        info.style.visibility = "visible"
         buttons[0].style.visibility = "visible"
         hidechoices()
     }
@@ -75,19 +98,18 @@ var pc = 0;
 var scoremsg = document.querySelector('#score')
 var humanscore = document.querySelector('.humanscore')
 var pcscore = document.querySelector('.pcscore')
-
-
+var info = document.querySelector(".info")
 var buttons = document.querySelectorAll("button")
 var scoreboard = document.querySelectorAll('.score')
 var choicebuttons = document.querySelectorAll('.choices')
+
 
 
 function hidechoices() {
     choicebuttons.forEach(button => {
         button.style.visibility = "hidden"
     }
-)
-}
+)}
 
 hidechoices()
 
@@ -98,7 +120,7 @@ if (buttons) {
             console.log(this.className)
             if (this.className == "start" && started == 0) {
                 started = 1;
-                document.querySelector(".info").textContent = ""
+                info.textContent = ""
                 this.style.visibility = "hidden"
                 scoremsg.textContent = "Score:"
                 scoreboard.forEach(scoreboard => { scoreboard.style.visibility = "visible" })
